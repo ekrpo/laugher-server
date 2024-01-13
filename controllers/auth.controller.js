c552975d3dc255eccf270f552240c34f19e17fcf
ekrpo <139063471+ekrpo@users.noreply.github.com>
2024-01-13 01:08:00 +0100

    cookie update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -117,14 +117,14 @@ export async function signIn(req, res, next) {
       httpOnly: true,
       sameSite: "None",
       secure: true,
-      path: "/"
+      domain: ".onrender.com"
     })
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       sameSite: "None",
       secure: true,
-      path: "/",
+      domain: ".onrender.com"
     })
 
     const userId = req.userId
@@ -208,7 +208,7 @@ export async function refreshToken(req, res, next) {
 
     res.cookie("accessToken", newAccessToken, {
       httpOnly: true,
-      path: "/",
+      domain: ".onrender.com"
       sameSite: "None",
       secure: true,
       maxAge: 1000 * 60 * 60
@@ -216,7 +216,7 @@ export async function refreshToken(req, res, next) {
 
     res.cookie("refreshToken", newRefreshToken, {
       httpOnly: true,
-      path: "/",
+      domain: ".onrender.com"
       sameSite: "None",
       secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 7
